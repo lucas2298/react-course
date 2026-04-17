919abeda84273ffbdd50aade683d50d07badea52
Thang Ha Tien <hatien.thang@heineken.com>
2026-04-17 16:42:01 +0700

    update alot

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,14 +3,23 @@ import { CheckoutPage } from "./pages/CheckoutPage";
 import { Routes, Route } from "react-router";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/cart-items?expand=product")
+      .then((response) => setCartItems(response.data));
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/" element={<HomePage cartItems={cartItems} />} />
+      <Route path="/checkout" element={<CheckoutPage cartItems={cartItems} />} />
+      <Route path="/orders" element={<OrdersPage cartItems={cartItems} />} />
       <Route path="/tracking" element={<TrackingPage />} />
     </Routes>
   );
